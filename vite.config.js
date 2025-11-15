4e550ed60c63446b697f9b45c72c2233bcb930ae
ArnoBoriau <arno_boriau@icloud.com>
2025-11-15 12:37:03 +0100

    Add comment to clarify base public path for deployment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-hashrouter-boilerplate/",
+  base: "/react-hashrouter-boilerplate/", // Base public path for deployment. Use "/repo-name/" for GitHub Pages or "./" for relative/local/static hosting.
 });
